3bc48bbb5199669fb3e582739e54c6083bb32871
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-10-29 23:12:54 -0500

    added start function to prompt options

--- connection.js
+++ connection.js
@@ -17,4 +17,5 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  start();
 });
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var mysql = require("mysql");
+var inquirer = require ("inquirer")
+var connection = ("./connection")
 
-var connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "password",
-  database: "employeesDB"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-});
+function start(){
+    inquirer
+    .prompt({
+        type: "list",
+        name: "name",
+        message: "What would you like to do?",
+        choices: [
+            "Add Department",
+            "Add Employee",
+            "Add Role",
+            "View Departments",
+            "View Employees",
+            "View Roles",
+            "Update Employee Roles",
+            "View Total Utilized Budget for Department"
+        ]
+    })
+}
