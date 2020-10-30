ffd7fbf43b456d47b58ca8bcae596400d965e2b5
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-10-29 23:27:51 -0500

    moved connection to server.js

--- connection.js
+++ connection.js
@@ -1,16 +1,19 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "employee_tracker"
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employeesDB"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+connection.connect(function(err) {
+  if (err) throw err;
+  start();
+  console.log("connected as id " + connection.threadId);
 });
 
-module.exports = connection
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var inquirer = require ("inquirer");
-var connection = require ("./connection");
+//PROBLEM: getting errors or nothing returned when using connection.js
+// var connection = require ("./connection");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employeesDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  start();
+  console.log("connected as id " + connection.threadId);
+});
 
 function start(){
     inquirer
