fbd1f303b635b6436a23eff97334e6b8516c8ad2
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-12-02 22:53:53 -0600

    minor changes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "password",
   database: "employeesDB",
 });
 
